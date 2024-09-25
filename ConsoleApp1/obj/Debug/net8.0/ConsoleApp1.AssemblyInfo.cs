2ae48508b82276445a9fba859bc24e1ec2ab15fc
goldenstarinc <goldenstar.inc@yandex.ru>
2024-09-25 22:23:19 +0300

    subtle changes

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdc25d0e602cf4319c93407078c5aea0db9c4202")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f56955ce69903b57fa2c15f57dcd541fa8b12302")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
