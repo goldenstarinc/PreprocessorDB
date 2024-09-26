ff5f74f8957cfa8175c14551c0dcbcff47775c22
goldenstarinc <goldenstar.inc@yandex.ru>
2024-09-26 19:00:02 +0300

    Summary?

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a97917b5db7808fc03e4aea1878b12de24626a35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d70ce10cec9334eca36d8ed0951402e00f18012")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a97917b5db7808fc03e4aea1878b12de24626a35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d70ce10cec9334eca36d8ed0951402e00f18012")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
